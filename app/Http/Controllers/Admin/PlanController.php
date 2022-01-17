6590d7182b6dcacaf06a2df1935df84a30a107ba
Eduardo Henrique Paludo <eduardopaludo@gmail.com>
2022-01-17 15:51:10 -0300

    Plans list

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Plan;
+
 class PlanController extends Controller
 {
+    private $repository;
+
+    public function __construct(Plan $plan){
+        $this->repository = $plan;
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,11 @@ class PlanController extends Controller
      */
     public function index()
     {
-        return view('admin.pages.plans.index');
+        $plans = $this->repository->latest()->paginate(1);
+
+        return view('admin.pages.plans.index', [
+            'plans' => $plans
+        ]);
     }
 
     /**
