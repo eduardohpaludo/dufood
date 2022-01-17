95fa4b77e87a638acb1e167b4d2ad238517f9234
Eduardo Henrique Paludo <eduardopaludo@gmail.com>
2022-01-17 14:27:26 -0300

    Updated menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'DuFood',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Du</b>Food',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'DuFood',
 
     /*
     |--------------------------------------------------------------------------
@@ -242,9 +242,9 @@ return [
             'text' => 'search',
         ],
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'text' => 'Planos',
+            'url'  => 'admin/plans',
+            //'can'  => 'manage-blog',
         ],
         [
             'text'        => 'pages',
